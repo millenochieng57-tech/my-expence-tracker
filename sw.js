a89a850e2c50510ca500fb69daf38e67e4323058
Millen Ochieng <millenochieng57@gmail.com>
2026-09-08 17:58:44 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open('millen-v2').then(cache => {
+    caches.open('MANAGER-v2').then(cache => {
       return cache.addAll(['./index.html','./style.css','./script.js','./manifest.json']);
     })
   );
