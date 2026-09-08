37091a55ac61140360508f0ddc59bb7b0174ef66
Millen Ochieng <millenochieng57@gmail.com>
2026-09-08 15:26:28 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,2 +1,30 @@
-self.addEventListener('install', e => self.skipWaiting());
-self.addEventListener('activate', e => self.clients.claim());
+const CACHE = 'millen-v1';
+const FILES = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => {
+    return Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)));
+  }));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      return cached || fetch(e.request).catch(() => caches.match('./index.html'));
+    })
+  );
+});
