96bede442bdd81b6f48fa1d68def8af892b40f1b
Millen Ochieng <millenochieng57@gmail.com>
2026-09-08 18:18:40 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,13 @@
+const CACHE_NAME = 'smart-manager-v5-FINAL';
+const FILES = ['./','./index.html','./style.css','./script.js','./manifest.json'];
+
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open('MANAGER-v2').then(cache => {
-      return cache.addAll(['./index.html','./style.css','./script.js','./manifest.json']);
-    })
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => { if(k!==CACHE_NAME) return caches.delete(k) }))));
 });
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
